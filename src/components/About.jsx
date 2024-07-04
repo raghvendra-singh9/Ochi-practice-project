2ecd758e79450820a945c785417a41b17222bddb
Raghvendra Singh Shekhawat <163868970+raghvendra-singh9@users.noreply.github.com>
2024-07-04 22:59:20 +0530

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="w-full p-20 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl text-black">
+      <h1 className="font-['Neue_Montreal'] text-[4vw] leading-[4.5vw]">
+        Ochi is a strategic partner for fast-growing tech businesses that need
+        to raise funds, sell products, explain complex ideas, and hire great
+        people.
+      </h1>
+      <div className="w-full flex gap-5 border-t-[1px] pt-5 mt-10 border-[#a1b562]">
+        <div className="w-1/2">
+          <h1 className="text-7xl">Our Approach</h1>
+          <button className="flex gap-10 items-center px-10 py-6 mt-8 bg-zinc-900 rounded-full text-white">
+            Read More
+            <div className="w-3 h-3 bg-zinc-100 rounded-full"></div>
+          </button>
+        </div>
+        <div className="w-1/2 h-[70vh] rounded-3xl bg-[#b0c859]"></div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,13 +1,35 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 function Marquee() {
   return (
     <div>
-      <div className="w-full h-[50vh] bg-[#004D43]">
-        <div className="text">
-          <h1>We Are Ochi</h1>
-          <h1>We Are Ochi</h1>
-          <h1>We Are Ochi</h1>
+      <div className="w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]">
+        <div className="text border-t-2 border-b-2 border-zinc-300 flex gap-10 whitespace-nowrap overflow-hidden ">
+          <motion.h1
+            initial={{ x: 0 }}
+            animate={{ x: "-100%" }}
+            transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+            className="text-[17vw] leading-none font-['Founders_Grotesk_x-Condensed'] font-semibold uppercase pt-0 -mb-[0vw]"
+          >
+            We Are Ochi
+          </motion.h1>
+          <motion.h1
+            initial={{ x: 0 }}
+            animate={{ x: "-100%" }}
+            transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+            className="text-[17vw] leading-none font-['Founders_Grotesk_x-Condensed'] font-semibold uppercase pt-0 -mb-[0vw]"
+          >
+            We Are Ochi
+          </motion.h1>
+          <motion.h1
+            initial={{ x: 0 }}
+            animate={{ x: "-100%" }}
+            transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+            className="text-[17vw] leading-none font-['Founders_Grotesk_x-Condensed'] font-semibold uppercase pt-0 -mb-[0vw]"
+          >
+            We Are Ochi
+          </motion.h1>
         </div>
       </div>
     </div>
